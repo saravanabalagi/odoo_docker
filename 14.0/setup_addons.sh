6b16df0c6b064dd08245c43190741cc2616ab46f
Saravanabalagi Ramachandran <saravanabalagi@gmail.com>
2023-07-22 20:54:18 +0100

    Update addons using addon_repos.csv

--- 14.0/setup_addons.sh
+++ 14.0/setup_addons.sh
@@ -3,48 +3,26 @@ set -e
 
 ADDONS_DIR=$PWD/addons
 ADDONS_REPO_DIR=$PWD/addon_repos
-DEFAULT_BRANCH=14.0
+ADDONS_CSV_FILE=$PWD/addon_repos.csv
 
 mkdir -p $ADDONS_DIR
 mkdir -p $ADDONS_REPO_DIR
 cd $ADDONS_REPO_DIR
 
-ODOOMATES_ODOOAPPS=https://github.com/odoomates/odooapps
-ODOOMATES_ODOOAPPS_BRANCH=$DEFAULT_BRANCH
-git clone $ODOOMATES_ODOOAPPS
-cd $(basename $ODOOMATES_ODOOAPPS)
-git checkout $ODOOMATES_ODOOAPPS_BRANCH
-cp -r */ $ADDONS_DIR
+skip_headers=1
+while IFS=, read -r GIT_REPO GIT_BRANCH GIT_DIR COPY_BLOBS; do
+    if ((skip_headers)); then ((skip_headers--)); continue; fi
 
-OCA_ACCOUNT_RECONCILE=https://github.com/OCA/account-reconcile
-OCA_ACCOUNT_RECONCILE_BRANCH=$DEFAULT_BRANCH
-git clone $OCA_ACCOUNT_RECONCILE
-cd $(basename $OCA_ACCOUNT_RECONCILE)
-git checkout $OCA_ACCOUNT_RECONCILE_BRANCH
-cp -r account_*/ $ADDONS_DIR
+    cd $ADDONS_REPO_DIR
+    echo "Pulling addons from $GIT_DIR"
+    [ -d $GIT_DIR ] || git clone $GIT_REPO $GIT_DIR
+    cd $GIT_DIR
+    git checkout $GIT_BRANCH
+    git pull origin $GIT_BRANCH
 
-OCA_ACCOUNT_FINANCIAL_REPORTING=https://github.com/OCA/account-financial-reporting
-OCA_ACCOUNT_FINANCIAL_REPORTING_BRANCH=$DEFAULT_BRANCH
-git clone $OCA_ACCOUNT_FINANCIAL_REPORTING
-cd $(basename $OCA_ACCOUNT_FINANCIAL_REPORTING)
-git checkout $OCA_ACCOUNT_FINANCIAL_REPORTING_BRANCH
-cp -r account_*/ $ADDONS_DIR
-cp -r mis_*/ $ADDONS_DIR
-cp -r partner_statement $ADDONS_DIR
-
-OCA_WEB=https://github.com/OCA/web
-OCA_WEB_BRANCH=$DEFAULT_BRANCH
-git clone $OCA_WEB
-cd $(basename $OCA_WEB)
-git checkout $OCA_WEB_BRANCH
-cp -r web_*/ $ADDONS_DIR
-cp -r support_branding $ADDONS_DIR
-
-OCA_BANK_STATEMENT_IMPORT=https://github.com/OCA/bank-statement-import
-OCA_BANK_STATEMENT_IMPORT_BRANCH=$DEFAULT_BRANCH
-git clone $OCA_BANK_STATEMENT_IMPORT
-cd $(basename $OCA_BANK_STATEMENT_IMPORT)
-git checkout $OCA_BANK_STATEMENT_IMPORT_BRANCH
-cp -r account_*/ $ADDONS_DIR
-
-
+    echo "Copying addons from $GIT_DIR"
+    for glob in $COPY_BLOBS; do
+        cp -r $glob $ADDONS_DIR
+    done
+    echo "Copy completed\n"
+done < $ADDONS_CSV_FILE
